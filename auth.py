0302fa4098cd1c89e8a402bc15127eb4a086281e
ganyuke <personalemailforedits@gmail.com>
2022-09-24 19:37:39 -0700

    fix auth

--- auth.py
+++ auth.py
@@ -26,7 +26,7 @@ def main():
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0',
             'Host': 'www.remind.com',
-            'Accept': 'application / json',
+            'Accept': 'application/json',
             'Accept-Language': 'en-US, en; q = 0.5',
             'Accept-Encoding': 'gzip, deflate, br',
             'Referer': 'https://www.remind.com/',
@@ -37,16 +37,22 @@ def main():
             'Remind101-Timezone-Id': 'America/Los_Angeles',
             'Remind101-Timezone-Offset': '-25200',
             'Origin': 'https://www.remind.com',
-            'Cookie': 'client_uuid=72e6f80b-7270-444f-b4f9-e78271822bcc; session_uuid=2e4808a4-ab87-4ad6-bc29-e1ddba462d66; _dd_s=logs=1&id=2ea746d8-fcad-43df-95ce-fb5a5e7fb862&created=1664069763577&expire=1664070924968; persist=false; G_ENABLED_IDPS=google'
-        }
+            }
         r = session.get('https://www.remind.com/log_in/')
         r.html.render()
         print(r.html.find('#id-9', first=True))
         r = session.post(URL, data=json.dumps(login_data), headers=headers)
         print(r.text)
-        # Try accessing a page that requires you to be logged in
-        r = session.get('https://www.remind.com/v2/access_tokens/confirmed_login', headers=headers)
-        print(r.text)
+        print(r.status_code)
+        if r.status_code == 403:
+            emailVerify = input("Enter email verification code: ")
+            email_data = {
+                "address": EMAIL,
+                "code": emailVerify,
+            }
+            emailEndpoint = "https://www.remind.com/v2/devices/outbound_verification"
+            verify = session.post(emailEndpoint, data=json.dumps(email_data), headers=headers)
+            print(verify.text)
 
 if __name__ == '__main__':
     main()
